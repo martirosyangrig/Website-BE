4b4894bead364a748e6ac8b26a43743498d58f72
Grigor <107695412+martirosyangrig@users.noreply.github.com>
2023-04-10 13:59:48 +0400

    Update mySql.ts

--- src/config/mySql.ts
+++ src/config/mySql.ts
@@ -4,10 +4,10 @@ import { Post } from "../entity/posts/posts-entity";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "aws.connect.psdb.cloud",
     port: 3306,
-    username: "root",
-    password: "",
+    username: "r9i066i647nwt4p64978",
+    password: "pscale_pw_7DePWrFbPBfQa9ba5kRZCGjdaFqi2teqvksB8DW29JK",
     database: "mywebsite",
     entities: [User, Post],
     synchronize: true,
