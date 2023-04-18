b8c7efadddaf813312dfc1226c6d8e86189de466
Grigor <107695412+martirosyangrig@users.noreply.github.com>
2023-04-18 11:39:46 +0400

    Update mySql.ts

--- src/config/mySql.ts
+++ src/config/mySql.ts
@@ -4,7 +4,7 @@ import { Post } from "../entity/posts/posts-entity";
 
 export const AppDataSource = new DataSource({
      type: "postgres",
-     host: "dpg-cgv3j04s3fvmvqag17sg-a.frankfurt-postgres.render.com/",
+     host: "dpg-cgv3j04s3fvmvqag17sg-a.frankfurt-postgres.render.com",
      port: 5432,
   username: "grig",
   password: "QEMaOaoi6UyeiyTaVX89AlwJfx1o275v",
