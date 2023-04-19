d21bf2e517aa6611d0f1590083e75cad3f87e741
martirosyangrig <grigor.martriosyan2000@mail.ru>
2023-04-19 21:17:15 +0400

    connected hosted pg

--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -2,13 +2,16 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "dpg-cgv3j04s3fvmvqag17sg-a.frankfurt-postgres.render.com",
     port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "mywebsite",
+    username: "grig",
+    password: "QEMaOaoi6UyeiyTaVX89AlwJfx1o275v",
+    database: "mywebsite_dr6t",
     entities: ["src/entity/**/*.ts"],
     migrations: ["src/migrations/*.ts"],
     synchronize: false,
-    logging: false
+    logging: false,
+    ssl: {
+        rejectUnauthorized: true
+    },
 });
